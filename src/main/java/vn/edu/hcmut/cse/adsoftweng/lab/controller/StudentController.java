91004f15347f6d6caa7f7c123a039cd0d2b53590
minhtoan-nmt <nguyenminhtoan204@gmail.com>
2026-02-26 14:31:36 +0700

    lab 1 and lab 2 commited

---
+++ src/main/java/vn/edu/hcmut/cse/adsoftweng/lab/controller/StudentController.java
@@ -0,0 +1,32 @@
+package vn.edu.hcmut.cse.adsoftweng.lab.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.edu.hcmut.cse.adsoftweng.lab.entity.Student;
+import vn.edu.hcmut.cse.adsoftweng.lab.service.StudentService;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+    @Autowired
+    private StudentService service;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Student getMethodName(@PathVariable String id) {
+        return service.getById(id);
+    }
+    
+}
